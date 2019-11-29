069b83b295d924b2359e5e1d9c95f64fa0d7a1cb
An Huy <huy.nguyen@wearefram.com>
2019-11-29 11:21:49 +0700

    Fix module folder

--- src/Margins/Model/Logger/Logger.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Kodbruket\Margins\Model\Logger;
-
-class Logger extends \Monolog\Logger
-{
-}
--- src/Margins/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Kodbruket_Margins',
-    __DIR__
-);
